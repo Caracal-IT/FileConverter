9267a6c2e48b37c2edf06fbae89d48ab87f0720b
Ettiene.M <ettienemare@gmail.com>
2016-11-18 14:36:08 +0200

    Finnish the parser

---
+++ FileConverter/Source/Libraries/Caracal.FileConverter.Parser/CsvParser.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Caracal.FileConverter.CsvParser {
+    public class CsvParser {
+        public static DataTable Parse(string csvText) {
+            var table = new DataTable();
+            var lines = csvText?.Split('\n');
+
+            if (hasContent()) {
+                ParseHeader();
+                ParseRows();
+            }
+
+            return table;
+
+            bool hasContent() => csvText?.Length > 0 && lines != null && lines.Count() > 0;
+
+            void ParseHeader() => ParseRowIntoList(lines[0], table.Headers);                
+
+            void ParseRows() {
+                for (int i = 1; i < lines.Length; i++) {
+                    var row = new List<string>();
+                    ParseRowIntoList(lines[i], row);
+                    table.Rows.Add(row);                
+                }
+            }
+
+            void ParseRowIntoList(string row, IList<string> list){
+                Regex.Split(row, ",")
+                     .Select(h => h.Trim())
+                     .ToList()
+                     .ForEach(h => list.Add(h));
+            }
+        }
+    }
+
+    public class DataTable {
+        public IList<string> Headers { get; set; }
+        public IList<IList<string>> Rows { get; set; }
+
+        public DataTable() {
+            Headers = new List<string>();
+            Rows = new List<IList<string>>();
+        }
+    }
+}
---
+++ FileConverter/Tests/Libraries/Caracal.FileConverter.Parser.Tests/ParserTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using Xunit;
+
+using static Xunit.Assert;
+
+namespace Caracal.FileConverter.CsvParser.Tests {
+    public class ParserTests {
+        [Fact]
+        public void ParseNull() {
+            var t = CsvParser.Parse(null);
+
+            False(t.Headers.Any());
+            False(t.Rows.Any());
+        }
+
+        [Fact]
+        public void ParseEmpty() {
+            var t = CsvParser.Parse(string.Empty);
+
+            False(t.Headers.Any());
+            False(t.Rows.Any());
+        }
+
+        [Fact]
+        public void ParseWithOneHeaderAndNoRows() {
+            var t = CsvParser.Parse("Header1");
+
+            Equal(1, t.Headers.Count());
+            Equal("Header1", t.Headers[0]);
+            False(t.Rows.Any());
+        }
+
+        [Fact]
+        public void ParseWithTwoHeaders() {
+            var t = CsvParser.Parse(" Header1 , Header2 ");
+
+            Equal(2, t.Headers.Count());
+            Equal("Header1", t.Headers[0]);
+            Equal("Header2", t.Headers[1]);
+            False(t.Rows.Any());
+        }
+
+        [Fact]        
+        public void ParseWithOneHeaderAndOneRow() {
+            var t = CsvParser.Parse("Header1\nRow1Col1");
+
+            Equal(1, t.Headers.Count());
+            Equal("Header1", t.Headers[0]);
+            Equal(1, t.Rows.Count());
+            Equal("Row1Col1", t.Rows[0][0]);
+        }
+
+        
+        [Fact]
+        public void ParseWithMultipleHeaderAndRows() {
+            var t = CsvParser.Parse("Header1 , Header2\nRow1Col1, Row1Col2 \nRow2Col1,Row2Col2");
+
+            Equal(2, t.Headers.Count());
+            Equal("Header1", t.Headers[0]);
+            Equal("Header2", t.Headers[1]);
+
+            Equal(2, t.Rows.Count());
+            Equal("Row1Col1", t.Rows[0][0]);
+            Equal("Row1Col2", t.Rows[0][1]);
+
+            Equal("Row2Col1", t.Rows[1][0]);
+            Equal("Row2Col2", t.Rows[1][1]);
+        }        
+    }
+}
