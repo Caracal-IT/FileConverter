1d18cac9fdf2fc9ef26cf9d68025201189b026e3
Ettiene.M <ettienemare@gmail.com>
2016-11-18 14:41:23 +0200

    Move datatable to new file

--- FileConverter/Source/Libraries/Caracal.FileConverter.Parser/CsvParser.cs
+++ FileConverter/Source/Libraries/Caracal.FileConverter.Parser/CsvParser.cs
@@ -35,14 +35,4 @@ namespace Caracal.FileConverter.CsvParser {
             }
         }
     }
-
-    public class DataTable {
-        public IList<string> Headers { get; set; }
-        public IList<IList<string>> Rows { get; set; }
-
-        public DataTable() {
-            Headers = new List<string>();
-            Rows = new List<IList<string>>();
-        }
-    }
 }
---
+++ FileConverter/Source/Libraries/Caracal.FileConverter.Parser/DataTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Caracal.FileConverter.CsvParser {
+
+    public class DataTable {
+        public IList<string> Headers { get; set; }
+        public IList<IList<string>> Rows { get; set; }
+
+        public DataTable() {
+            Headers = new List<string>();
+            Rows = new List<IList<string>>();
+        }
+    }
+}
